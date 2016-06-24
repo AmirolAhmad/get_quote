3d69113b4d6044f67d9a7345c2c87f0fa3974a87
Amirol Ahmad <9w2pdf@gmail.com>
2016-06-24 14:31:38 +0800

    passed user rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :user do |n|
+    email {|n| "person#{n}@example.com" }
+    username {|n| "username#{n}"}
+    password 'secret123'
+    password_confirmation 'secret123'
+    confirmed_at Date.today
+    admin 0
+    factory :admin do
+      admin 1
+    end
   end
 end
