e96c2248a234c93c4733af15c3fe3fcb5ec335f1
Amirol Ahmad <9w2pdf@gmail.com>
2016-06-24 16:30:40 +0800

    setup user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get "/dashboard" => 'dashboards#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
